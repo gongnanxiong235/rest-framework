e833ac6e5e5ff1fbdde759243c54f5d2941109ec
Your Name <you@example.com>
2019-05-15 14:44:29 +0800

    'dd'

--- restapi/urls.py
+++ restapi/urls.py
@@ -8,7 +8,9 @@ urlpatterns = [
     re_path(r'^(?P<version>v[0-9].[0-9]+)/order/$', v.OrderView.as_view()),
     re_path(r'^(?P<version>v[0-9].[0-9]+)/userinfo/$', v.UserInfo.as_view()),
     re_path(r'^(?P<version>v[0-9].[0-9]+)/version/$', v.Version.as_view(),name='user'),
-    re_path(r'^(?P<version>v[0-9].[0-9]+)/parse/$', v.ParseView.as_view(),name='user'),
-    re_path(r'^(?P<version>v[0-9].[0-9]+)/role/$', v.Roleview.as_view(),name='user'),
-    re_path(r'^(?P<version>v[0-9].[0-9]+)/userdetail/$', v.UseretailView.as_view(),name='user'),
+    re_path(r'^(?P<version>v[0-9].[0-9]+)/parse/$', v.ParseView.as_view()),
+    re_path(r'^(?P<version>v[0-9].[0-9]+)/role/$', v.Roleview.as_view()),
+    re_path(r'^(?P<version>v[0-9].[0-9]+)/userdetail/$', v.UseretailView.as_view()),
+    re_path(r'^(?P<version>v[0-9].[0-9]+)/userdetail3/$', v.UserDetailLink.as_view()),
+    re_path(r'^(?P<version>v[0-9].[0-9]+)/group/(?P<pk>\d+)/userid_111111/$', v.GroupView.as_view(),name='gp'),
 ]
--- restapi/views.py
+++ restapi/views.py
@@ -4,24 +4,25 @@ from restapi import models
 from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.request import Request
-from rest_framework import exceptions
+from rest_framework import exceptions, serializers
 from rest_framework.authentication import BasicAuthentication
-import time, hashlib,json
+import time, hashlib, json
 from utils import permission
 from rest_framework.versioning import URLPathVersioning
-from rest_framework.parsers import JSONParser,FormParser
+from rest_framework.parsers import JSONParser, FormParser
 
 ORDER_DICT = [{
-    'order_id':1,
+    'order_id': 1,
     'name': 'iphonex max',
     'price': 9888,
     'color': 'red'
 }, {
-    'order_id':2,
+    'order_id': 2,
     'name': 'macbook',
     'price': 18000,
     'color': 'white'
 }]
+
 
 def md5(user_name):
     ctime = str(time.time())
@@ -31,20 +32,18 @@ def md5(user_name):
 
 
 # Create your views here.
-
 def create_user(user_type, user_name, password):
     models.UserInfo.objects.create(user_type=user_type, user_name=user_name, password=password)
 
 
-
 class AuthView(APIView):
-
     # 跳过认证
     authentication_classes = []
     # 跳过权限
     permission_classes = []
     # 跳过节流
     throttle_classes = []
+
     def post(self, request, *args, **kwargs):
         ret = {'code': 1000, 'msg': None}
         try:
@@ -70,13 +69,14 @@ class AuthView(APIView):
                     models.UserToken.objects.update(user_id=user_id, user_token=token)
                 ret['code'] = 1000
                 ret['msg'] = 'OK'
-                ret['data'] = {'token': token,"user_id":user_id}
+                ret['data'] = {'token': token, "user_id": user_id}
         except Exception as e:
             print(e)
             ret['code'] = 1002
             ret['msg'] = '请求异常'
 
         return JsonResponse(ret)
+
 
 # 用户认证token校验
 class OrderView(APIView):
@@ -94,113 +94,213 @@ class OrderView(APIView):
             ret['msg'] = '请求异常'
         return JsonResponse(ret)
 
+
 class UserInfo(APIView):
     # 局部权限
     permission_classes = [permission.SVIPPermission]
-    def post(self,request, *args, **kwargs):
+
+    def post(self, request, *args, **kwargs):
         ret = {'code': 1000, 'msg': 'UserInfo'}
         return JsonResponse(ret)
 
 
 class Version(APIView):
     # 跳过认证
-    authentication_classes=[]
+    authentication_classes = []
     # 跳过权限
-    permission_classes=[]
+    permission_classes = []
     # 跳过节流
-    throttle_classes=[]
+    throttle_classes = []
+
     def get(self, request, *args, **kwargs):
         # 获取版本，如果没有加版本默认为v1  在setting中可以进行默认版本的配置
         print(request.version)
         # 反向获取请求的地址 第一个参数是在urels中配置路由 后面的name  re_path(r'^(?P<version>v[0-9].[0-9]+)/version/$', v.Version.as_view(),name='user'),
-        print(request.versioning_scheme.reverse('user',request=request))
-        return HttpResponse((request.version,request.versioning_scheme.reverse('user',request=request)))
+        print(request.versioning_scheme.reverse('user', request=request))
+        return HttpResponse((request.version, request.versioning_scheme.reverse('user', request=request)))
 
 
 class ParseView(APIView):
-    authentication_classes=[]
-    permission_classes=[]
+    authentication_classes = []
+    permission_classes = []
     '''
     如果列表中是JSONParser:表示请求的时候必须以json格式请求 content-type:application/json,
     request._request.POST没数据,如果Header中 content-type:不是application/json 则会报错
     '''
+
     # parser_classes = [JSONParser,FormParser]
-    def post(self,request, *args, **kwargs):
-        print('post:',request._request.POST)
-        print ( 'haha', request._request.POST.get( 'name' ,None) )
+    def post(self, request, *args, **kwargs):
+        print('post:', request._request.POST)
+        print('haha', request._request.POST.get('name', None))
         # request.data 只能返回post请求的数据
-        print('data',request.data)
-        print('haha',request.data.get('name'))
+        print('data', request.data)
+        print('haha', request.data.get('name'))
         return HttpResponse('hello')
 
 
-from rest_framework import serializers
 class RoleSerializers(serializers.Serializer):
-    id=serializers.IntegerField()
-    title=serializers.CharField()
+    id = serializers.IntegerField()
+    title = serializers.CharField()
+
 
 class Roleview(APIView):
-    authentication_classes=[]
-    permission_classes=[]
-    def get(self,request, *args, **kwargs):
-        ret={'code': 1000, 'msg': None, 'data': None}
+    authentication_classes = []
+    permission_classes = []
+
+    def get(self, request, *args, **kwargs):
+        ret = {'code': 1000, 'msg': None, 'data': None}
         '''方式一序列化'''
         # roles=models.UserRole.objects.all().values('id','title')
         # role_list=list(roles)
 
         '''方式二序列化'''
-        roles=models.UserRole.objects.all()
-        ser=RoleSerializers(instance=roles,many=True)
-        data=json.dumps(ser.data,ensure_ascii=False) # 显示中文
+        roles = models.UserRole.objects.all()
+        ser = RoleSerializers(instance=roles, many=True)
+        data = json.dumps(ser.data, ensure_ascii=False)  # 显示中文
 
-        ret['data']=data
+        ret['data'] = data
         print(ret)
         return JsonResponse(ret)
 
-class UserInfoSerializers(serializers.Serializer):
-    user_id=serializers.IntegerField(source='id')
-    # user_type=serializers.ChoiceField(choices=((1,'普通用户'),(2,'VIP用户'),(3,'超级VIP 用户')))
-    user_type=serializers.SerializerMethodField()
-    user_name=serializers.CharField()
-    group_name=serializers.SerializerMethodField()
-    roles=serializers.SerializerMethodField()
 
-    def get_roles(self,obj):
-        role_name=list()
-        user_id=obj.id
-        role_ids=models.UserinfoRoles.objects.filter(user_id=user_id).values('role_id')
+# 可以对序列化字段的值进行加工和定制  == user_name = serializers.CharField()
+class MyField(serializers.CharField):
+    def to_representation(self, value):
+        if value == 'gongnanxiong':
+            return 'shuaige:' + value
+        return value
+
+
+# 序列化 方式1
+class UserInfoSerializers(serializers.Serializer):
+    user_id = serializers.IntegerField(source='id')
+    # user_type=serializers.ChoiceField(choices=((1,'普通用户'),(2,'VIP用户'),(3,'超级VIP 用户')))
+    user_type = serializers.SerializerMethodField()
+    # user_name = serializers.CharField()
+    user_name = MyField()
+    group_name = serializers.SerializerMethodField()
+    roles = serializers.SerializerMethodField()
+
+    def get_roles(self, obj):
+        role_name = list()
+        user_id = obj.id
+        role_ids = models.UserinfoRoles.objects.filter(user_id=user_id).values('role_id')
         for rid in role_ids:
-            role_obj=models.UserRole.objects.filter(id=rid.get('role_id')).first()
+            role_obj = models.UserRole.objects.filter(id=rid.get('role_id')).first()
             role_name.append(role_obj.title)
         return role_name
 
-    def get_user_type(self,obj):
-        type=obj.user_type
-        if type==1:
+    def get_user_type(self, obj):
+        type = obj.user_type
+        if type == 1:
             return '普通用户'
-        elif type==2:
+        elif type == 2:
             return 'vip用户'
-        elif type==3:
+        elif type == 3:
             return ' 超级VIP用户'
 
-    def get_group_name(self,obj):
-        print('obj',obj)
+    def get_group_name(self, obj):
+        print('obj', obj)
         if obj.group_id:
             print(obj.id)
-            group=models.UserGroup.objects.filter(id=obj.group_id).first()
+            group = models.UserGroup.objects.filter(id=obj.group_id).first()
             if group:
-                return {"id":group.id,'title':group.title}
+                return {"id": group.id, 'title': group.title}
         return None
 
+
+# 序列化 方式2
+class UserInfoSerializers2(serializers.ModelSerializer):
+    group_name = serializers.SerializerMethodField()
+    user_type = serializers.SerializerMethodField()
+    roles = serializers.SerializerMethodField()
+
+    class Meta:
+        model = models.UserInfo
+        # fields = "__all__"
+        fields = ['id', 'user_name', 'user_type', 'group_name', 'roles']
+        # depth = 1   有关联关系的自动查询
+
+    def get_roles(self, obj):
+        role_name = list()
+        user_id = obj.id
+        role_ids = models.UserinfoRoles.objects.filter(user_id=user_id).values('role_id')
+        for rid in role_ids:
+            role_obj = models.UserRole.objects.filter(id=rid.get('role_id')).first()
+            role_name.append(role_obj.title)
+        return role_name
+
+    def get_user_type(self, obj):
+        type = obj.user_type
+        if type == 1:
+            return '普通用户'
+        elif type == 2:
+            return 'vip用户'
+        elif type == 3:
+            return ' 超级VIP用户'
+
+    def get_group_name(self, obj):
+        print('obj', obj)
+        if obj.group_id:
+            print(obj.id)
+            group = models.UserGroup.objects.filter(id=obj.group_id).first()
+            if group:
+                return {"id": group.id, 'title': group.title}
+        return None
+
+
 class UseretailView(APIView):
-    authentication_classes=[]
-    permission_classes=[]
-    def get(self,request, *args, **kwargs):
-        ret={"code": 1000, "msg": None, "data": None}
-        data=json.dumps(UserInfoSerializers(models.UserInfo.objects.all().first(),many=False).data,ensure_ascii=False)
-        ret['data']=data
-        print(JsonResponse ( ret ).content)
+    authentication_classes = []
+    permission_classes = []
+
+    def get(self, request, *args, **kwargs):
+        ret = {"code": 1000, "msg": None, "data": None}
+        data = json.dumps(UserInfoSerializers(models.UserInfo.objects.all(), many=True).data, ensure_ascii=False)
+        ret['data'] = data
+        print(JsonResponse(ret).content)
         # return JsonResponse ( ret )
         return HttpResponse(data)
 
 
+# 序列化 方式3  反向生成url的方式
+class UserInfoSerializers3(serializers.ModelSerializer):
+    # lookup_field='group_id',lookup_url_kwarg='pk':按照对用的group_id去生成url  如果不写 默认按照userinfo的id来生成
+    group = serializers.HyperlinkedIdentityField(view_name='gp', source='group_id', lookup_field='group_id',
+                                                 lookup_url_kwarg='pk')
+
+    class Meta:
+        model = models.UserInfo
+        fields = "__all__"
+
+
+class GroupSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = models.UserGroup
+        fields = "__all__"
+
+
+class GroupView(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    def get(self, request, *args, **kwargs):
+        print('args',args)
+        print('kwargs',kwargs)
+        print('get',request._request.GET)
+        pk = kwargs.get('pk')
+        obj = models.UserGroup.objects.filter(id=pk).first()
+        ser = GroupSerializers(instance=obj, many=False)
+        ret = json.dumps(ser.data, ensure_ascii=False)
+        return HttpResponse(ret)
+
+
+class UserDetailLink(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    def get(self, request, *args, **kwargs):
+        obj = models.UserInfo.objects.all()
+        # 如果需要反向生成URL  必须加上context={'request': request}
+        ser = UserInfoSerializers3(instance=obj, many=True, context={'request': request})
+        ret = json.dumps(ser.data, ensure_ascii=False)
+        return HttpResponse(ret)
